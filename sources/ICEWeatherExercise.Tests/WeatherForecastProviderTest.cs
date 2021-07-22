14df7517e651bf051f869788239f8d42e57f23de
Lev Strelnikov <levkaa@gmail.com>
2021-07-23 01:02:32 +0300

    Added tests

---
+++ sources/ICEWeatherExercise.Tests/WeatherForecastProviderTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using ICEWeatherExercise.Contracts;
+using ICEWeatherExercise.Contracts.Storages;
+using ICEWeatherExercise.Core;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace ICEWeatherExercise.Tests
+{
+    public class WeatherForecastProviderTest
+    {
+        [Fact]
+        public void ResolveForecastFileParametersTest()
+        {
+            WeatherForecastProvider forecastProvider = new WeatherForecastProvider(Mock.Of<ILocalFileStorage>(),
+                Mock.Of<IRemoteFileStorage>(), Mock.Of<IWeatherForecastFileParser>(),
+                Mock.Of<ILogger<WeatherForecastProvider>>());
+
+            var datetime = DateTime.UtcNow.Date - TimeSpan.FromDays(1) + TimeSpan.FromHours(5);
+            var actual = forecastProvider.ResolveForecastFileParameters(datetime);
+            var expected = (DateTime.UtcNow.Date - TimeSpan.FromDays(1), 5);
+
+            Assert.Equal(expected, actual);
+
+
+            datetime = DateTime.UtcNow.Date + TimeSpan.FromHours(5);
+            actual = forecastProvider.ResolveForecastFileParameters(datetime);
+            expected = (DateTime.UtcNow.Date, 5);
+
+            Assert.Equal(expected, actual);
+
+            datetime = DateTime.UtcNow.Date + TimeSpan.FromDays(1) + TimeSpan.FromHours(5);
+            actual = forecastProvider.ResolveForecastFileParameters(datetime);
+            expected = (DateTime.UtcNow.Date, 29); // 24 + 5
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
