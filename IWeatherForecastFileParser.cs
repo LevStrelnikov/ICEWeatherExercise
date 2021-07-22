9b15e1cced491df51000e94ef976f1bfeba3e51d
Lev Strelnikov <levkaa@gmail.com>
2021-07-23 01:05:38 +0300

    Remove file committed by mistake

--- IWeatherForecastFileParser.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace ICEWeatherExercise.Core.Contracts
-{
-    public interface IWeatherForecastFileParser
-    {
-        Task<double> GetTemperature(double lon, double lat, string weatherForecastFilePath);
-    }
-}
